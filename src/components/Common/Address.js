424f94302eed00314eff1ea67bcf1b9f42fdb90a
Pradeep Manjunath <53876288+pradeepm93969@users.noreply.github.com>
2026-03-10 08:39:21 +0400

    address

--- src/components/Common/Address.js
+++ src/components/Common/Address.js
@@ -7,7 +7,7 @@ const Address = () => {
       Mu2creations FZE
 Office -1st Floor, 122 Shaika Mhara Building 
 P.O. Box: 87556, Near Al Twar Center, Al Qusais 2, Dubai UAE
-Landline : +971048346701
+Landline : +971 04 834 6701
     </div>
   );
 };
