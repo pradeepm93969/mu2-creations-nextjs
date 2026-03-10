5a5191d821e7cb5d7ea0432d009d2b80aa32c0e6
Pradeep Manjunath <53876288+pradeepm93969@users.noreply.github.com>
2026-03-10 08:34:10 +0400

    address change

--- src/components/Common/Address.js
+++ src/components/Common/Address.js
@@ -4,7 +4,11 @@ const Address = () => {
   return (
     <div className="flex items-center justify-center gap-2">
       <FaLocationDot />
-      Sharjah – U.A.E (Serving Dubai, Abu Dhabi & beyond)
+      Mu2creations FZE
+Office -1st Floor, 122 Shaika Mhara Building 
+P.O. Box: 87556, Near Al Twar Center, Al Qusais 2, Dubai UAE
+Landline : +971048346701
+Mobile is same as old one
     </div>
   );
 };
