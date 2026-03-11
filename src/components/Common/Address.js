3bd0a9e9ee781228c90c2717c5ff86ec683b05bc
Pradeep Manjunath <pradeep.manjunath@goli.pk>
2026-03-11 05:17:59 +0400

    address

--- src/components/Common/Address.js
+++ src/components/Common/Address.js
@@ -2,12 +2,19 @@ import { FaLocationDot } from "react-icons/fa6";
 
 const Address = () => {
   return (
-    <div className="flex items-center justify-center gap-2">
+    <div className="flex items-center justify-center gap-2 max-w-sm">
       <FaLocationDot />
-      Mu2creations FZE
-Office -1st Floor, 122 Shaika Mhara Building 
-P.O. Box: 87556, Near Al Twar Center, Al Qusais 2, Dubai UAE
-Landline : +971 04 834 6701
+      <a
+    href="https://maps.app.goo.gl/4LTvKkzUqfHc2vxN8?g_st=aw"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="cursor-pointer"
+  >
+    Mu2creations FZE <br />
+    Office - 1st Floor, 122 Shaika Mhara Building <br />
+    P.O. Box: 87556, Near Al Twar Center, Al Qusais 2, Dubai UAE <br />
+    Landline : +971 04 834 6701
+  </a>
     </div>
   );
 };
